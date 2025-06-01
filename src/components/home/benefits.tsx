e6feb1533fd74f87d7ff8051fd4689d6a446e30f
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-06-01 09:30:43 +0100

    updated benefits

--- src/components/home/benefits.tsx
+++ src/components/home/benefits.tsx
@@ -4,25 +4,28 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { Badge } from "../ui/badge";
+import Benefit1 from "@/assets/images/benefit-1.avif";
+import Benefit2 from "@/assets/images/benefit-2.avif";
+import Benefit3 from "@/assets/images/benefit-3.avif";
 
 const benefits = [
   {
     title: "Effortless YT Management",
     description:
       "Our team handles everything from start to finish. Just sit back and watch your channel grow with minimal effort on your part.",
-    icon: "https://ext.same-assets.com/452876233/1509545232.png",
+    icon: Benefit1,
   },
   {
     title: "Extremely Fast Video Delivery Time",
     description:
       "Enjoy quick turnaround times on all your videos, ensuring your content schedule stays on track and consistent.",
-    icon: "https://ext.same-assets.com/452876233/1962855497.png",
+    icon: Benefit2,
   },
   {
     title: "Access to Private Discord + Team",
     description:
       "Join our exclusive Discord community and collaborate directly with our expert team for personalized support and guidance.",
-    icon: "https://ext.same-assets.com/452876233/2373779568.png",
+    icon: Benefit3,
   },
 ];
 
@@ -44,17 +47,21 @@ export default function Benefits() {
           {benefits.map((benefit, index) => (
             <div
               key={index}
-              className="flex flex-col border-2 border-black rounded-2xl p-6"
+              className="flex flex-col border-2 border-black shadow-[0_6px_0_0_#000000] rounded-2xl p-6"
             >
               <div className="mb-6">
-                <h3 className="text-4xl font-semibold mb-3">{benefit.title}</h3>
-                <Image
-                  src={benefit.icon}
-                  alt={benefit.title}
-                  width={100}
-                  height={100}
-                  className="h-full w-full object-contain"
-                />
+                <h3 className="text-4xl font-semibold mb-3 tracking-tighter">
+                  {benefit.title}
+                </h3>
+                <div className="flex justify-center">
+                  <Image
+                    src={benefit.icon}
+                    alt={benefit.title}
+                    width={400}
+                    height={400}
+                    className="h-[200px] w-[200px] object-contain"
+                  />
+                </div>
               </div>
               <p className="text-muted-foreground">{benefit.description}</p>
             </div>
@@ -63,7 +70,10 @@ export default function Benefits() {
 
         <div className="text-center">
           <Link href="/#call">
-            <Button className="btn-primary py-6 px-8 text-base">
+            <Button
+              variant={"threeD"}
+              className="btn-primary py-6 px-8 text-base"
+            >
               Book a Call
             </Button>
           </Link>
